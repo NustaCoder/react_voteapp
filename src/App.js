8c70fe1939d3ef894e280299c1cf97f735bd57a2
ShADow <shlokgadre@gmail.com>
2020-06-12 15:54:18 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
 import Main from "./components/MainComponent";
-import { BrowserRouter} from 'react-router-dom';
-import {Provider} from 'react-redux';
-import {ConfigureStore} from './redux/configureStore';
-
-const store =  ConfigureStore();
+import "./App.css";
 
 class App extends Component {
-
-  
+  constructor(props) {
+    super(props)
+    this.state = {
+      val: 10
+    }
+  }
   render() {
     return (
-      <Provider store={store}>
-      <BrowserRouter>
       <div>
-        <Main />
+        <Main val={this.state.val} />
       </div>
-      </BrowserRouter>
-      </Provider>
     );
   }
 }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,129 +1,46 @@
 import React, { Component } from "react";
-import Home from "./HomeComponent";
-import Menu from "./Menu";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-import About from "./AboutComponent";
-import Contact from "./ContactComponent";
-import {Switch,  Route, Redirect, withRouter } from 'react-router-dom';
-import { postComment, fetchDishes, fetchPromos,
- fetchComments, fetchLeaders} from '../redux/ActionCreators';
-import { actions} from 'react-redux-form';
-import { connect} from 'react-redux';
-import CommentForm from "./dishedetails";
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-
-const mapStateToProps = state => {
-    return {
-
-      dishes: state.dishes,
-      comments: state.comments,
-      promotions:state.promotions,
-      leaders:state.leaders
-    }
-}
-const mapDispatchToProps = (dispatch)=>({
-postComment: (dishId,rating, author, comment)=> dispatch(postComment(dishId,rating, author, comment)),
-fetchDishes: () => {dispatch(fetchDishes())},
-resetFeedbackForm: ()=> {dispatch(actions.reset('feedback'))},
-fetchComments: () => {dispatch(fetchComments())},
-fetchPromos: () => {dispatch(fetchPromos())},
-fetchLeaders: () => {dispatch(fetchLeaders())}
-})
-
-
 
 class Main extends Component {
-  constructor(props) {
-    super(props);
-    
-  }
-
-  
-componentDidMount() {
-  this.props.fetchDishes();
-  this.props.fetchComments();
-  this.props.fetchPromos();
-  this.props.fetchLeaders();
-}
-  
- 
-
- // callComponent(dish) {
-   // if (dish != null) {
-    //  return (
-    //    <DishDetails
-      //    selectedDish={
-         //   this.state.dishes.filter(
-         //     dish => dish.id === this.state.selectedDish
-           // )[0]
-      //    }
-      //  />
-     // );
-   // }
- // }
-  render() {
-
-    const HomePage=()=> {
-      return(
-        <Home dish={this.props.dishes.dishes.filter((dish)=>dish.featured)[0]}
-        dishesLoading={this.props.dishes.isLoading}
-        dishesErrMess={this.props.dishes.errmess}
-
-        promotion ={this.props.promotions.promotions.filter((promo)=>promo.featured)[0]}
-        promosLoading={this.props.promotions.isLoading}
-        promosErrMess={this.props.promotions.errmess}
-
-        leader ={this.props.leaders.leaders.filter((leader)=>leader.featured)[0]}
-        leadersLoading={this.props.leaders.isLoading}
-        leadersErrMess={this.props.leaders.errmess}
-        />
-      );
+    constructor(props) {
+        super(props);
+        this.state = {
+            Team_IronMan: 0,
+            Team_Captain: 0
+        }
     }
 
-  const DishWithId =  ({match}) => {
+    giveVote(team) {
+        if (team == 1) {
+            this.setState({
+                Team_IronMan: this.state.Team_IronMan + 1
+            });
+        }
+        else if (team == 2) {
+            this.setState({
+                Team_Captain: this.state.Team_Captain + 1
+            });
+        }
+    }
 
-    return(
+    render() {
+        return (
+            <div>
+                <Header />
+                <div class="container">
+                    <div class="row row-content  ">
+                        <div class="col-12 col-sm-4 ironman align-middle">
+                            <button class="align-items-center" onClick={this.giveVote.bind(this, 1)}>Team IronMan : {this.state.Team_IronMan}</button>
+                        </div>
+                        <div class="col-12 col-sm-4 captain text-center" onClick={this.giveVote.bind(this, 2)}>
+                            <button>Team Captain : {this.state.Team_Captain}</button>
+                        </div>
+                    </div>
 
-      <CommentForm dish={this.props.dishes.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]}
-          isLoading={this.props.dishes.isLoading}
-          errmess={this.props.dishes.errmess}
-            comments={this.props.comments.comments.filter((comments) => comments.dishId === parseInt(match.params.dishId,10))}
-            commentsErrMess={this.props.comments.errmess}
-            postComment={this.props.postComment}
-      />
-    );
-
-  }
-
-  const AboutUs = () => {
-    return (
-      <About leaders={this.props.leaders} />
-    );
-  }
-
-
-    return (
-      <div>
-        <Header />
-
-        <TransitionGroup>
-          <CSSTransition key = {this.props.location.key} 
-          classNames = "page" timeout = {300}>
-        <Switch>
-           <Route path="/home" component={HomePage}/>
-           <Route exact path="/menu" component={() => <Menu dishes={this.props.dishes}/> }/>  
-            <Route exact path="/contactus" component={()=> <Contact resetFeedbackForm={this.props.resetFeedbackForm}/>}/>
-            <Route path = "/menu/:dishId" component={DishWithId}/>
-            <Route path = "/aboutus" component={AboutUs}/>
-            <Redirect to="/home"/>
-        </Switch>
-        </CSSTransition>
-        </TransitionGroup>
-        <Footer />
-      </div>
-    );
-  }
+                </div>
+            </div>
+        );
+    }
 }
-
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
+export default Main;
